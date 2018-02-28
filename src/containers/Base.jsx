7dd88b546727a68930dd648db4aba6efe2b724cd
romgar <romain.garrigues.cs@gmail.com>
2018-02-28 21:57:42 +0000

    Add navigation buttons between homepage and my-account page

--- src/containers/Base.jsx
+++ src/containers/Base.jsx
@@ -1,20 +1,38 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import AuthStatus from './AuthStatus';
 import LoginForm from './LoginForm';
+import LogoutForm from './LogoutForm';
 
 class Base extends React.Component {
   render() {
+    const { loggedIn } = this.props;
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <AuthStatus />
-        <LoginForm />
+        {loggedIn ? (
+          <React.Fragment>
+            <Link to="/my-account">My account</Link>
+            <LogoutForm />
+          </React.Fragment>
+        ) : (
+          <LoginForm />
+        )}
       </div>
     );
   }
 }
 
-export default Base;
+const mapStateToProps = state => {
+  return {
+    loggedIn: state.auth.logged
+  };
+};
+
+export default connect(mapStateToProps)(Base);
--- src/containers/MyAccount.jsx
+++ src/containers/MyAccount.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import LogoutForm from './LogoutForm';
 
-class Base extends React.Component {
+class MyAccount extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">My account!</h1>
         </header>
+        <Link to="/">Homepage</Link>
         <LogoutForm />
       </div>
     );
   }
 }
 
-export default Base;
+export default MyAccount;

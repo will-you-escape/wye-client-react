5bdb5736a4c593ac473e76f0020962999cbf9726
romgar <romain.garrigues.cs@gmail.com>
2018-03-04 14:39:10 +0000

    Add the most minimal CSS to emulate a popup

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import CloseSign from './CloseSign';
+import './Overlay.css';
 
 class Overlay extends React.Component {
   render() {

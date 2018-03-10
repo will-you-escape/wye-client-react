53ce252b4912630a4543a38fbb752a61da9c19af
romgar <romain.garrigues.cs@gmail.com>
2018-03-10 12:01:16 +0000

    Plug header into the real App

--- src/containers/Base.jsx
+++ src/containers/Base.jsx
@@ -1,29 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 
-import AuthStatus from './AuthStatus';
-import LoginForm from './LoginForm';
-import LogoutForm from './LogoutForm';
+import Header from './header/Header';
 
 class Base extends React.Component {
   render() {
     const { loggedIn } = this.props;
 
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <AuthStatus />
-        {loggedIn ? (
-          <React.Fragment>
-            <Link to="/my-account">My account</Link>
-            <LogoutForm />
-          </React.Fragment>
-        ) : (
-          <LoginForm />
-        )}
+      <div className="app">
+        <Header loggedIn={loggedIn} />
       </div>
     );
   }

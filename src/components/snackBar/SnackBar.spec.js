6b20625e6c7d532106dc87328e1768719084a1fd
romgar <romain.garrigues.cs@gmail.com>
2018-08-05 12:21:28 +0100

    Update tests to cover situations where the snackbar should not be displayed

--- src/components/snackBar/SnackBar.spec.js
+++ src/components/snackBar/SnackBar.spec.js
@@ -30,6 +30,33 @@ describe('<SnackBar/>', () => {
     );
   });
 
+  it('does not render SnackBar if no notification', () => {
+    let store = wyeCreateStore();
+    const wrapper = mount(givenDefaultSnackBar(store));
+
+    expect(findSnackBarTitle(wrapper).exists()).to.be.false;
+    expect(findSnackBarMessage(wrapper).exists()).to.be.false;
+  });
+
+  it('does not render SnackBar if notification should not be displayed', () => {
+    const initialState = {
+      snackBar: {
+        shouldDisplay: false,
+        content: {
+          title: 'Internal server error',
+          message:
+            'An internal server error occured. Please retry again later.',
+          severity: 'error'
+        }
+      }
+    };
+    let store = wyeCreateStore(initialState);
+    const wrapper = mount(givenDefaultSnackBar(store));
+
+    expect(findSnackBarTitle(wrapper).exists()).to.be.false;
+    expect(findSnackBarMessage(wrapper).exists()).to.be.false;
+  });
+
   function findSnackBarTitle(wrapper) {
     return wrapper.find('.snackbar__title');
   }

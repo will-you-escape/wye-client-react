e36f1d35b260f4f4edf5f3dff6060c776b440bd6
romgar <romain.garrigues.cs@gmail.com>
2019-10-29 22:03:07 +0000

    Updates webpack storybook config according to storybook 5.X version

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,25 +1,36 @@
-'use strict';
+"use strict";
 
-const webpack = require('webpack');
-const path = require('path');
-const defaultConfigGenerator = require('@storybook/react/dist/server/config/defaults/webpack.config.js');
+const webpack = require("webpack");
+const path = require("path");
 
-module.exports = (baseConfig, env) => {
-    const config = defaultConfigGenerator(baseConfig, env);
+module.exports = ({ config, mode }) => {
+  config.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    use: [
+      {
+        loader: require.resolve("awesome-typescript-loader")
+      },
+      // Optional
+      {
+        loader: require.resolve("react-docgen-typescript-loader")
+      }
+    ]
+  });
+  config.resolve.extensions.push(".ts", ".tsx");
 
-    addFakeTestSuitePlugin(config);
+  addFakeTestSuitePlugin(config);
 
-    return config;
+  return config;
 };
 
 function addFakeTestSuitePlugin(config) {
-    // This "faking" of describe allows us to directly import some code from
-    // test files into storybook.
-    // The idea is to create a default component that will be firs tested,
-    // then imported in storybook to be displayed.
-    const fakeTestSuitePath = path.join(__dirname, 'fakeTestSuite.js');
-    const testSuiteProvider = new webpack.ProvidePlugin({
-        'describe': [fakeTestSuitePath, 'describe']
-    });
-    config.plugins.push(testSuiteProvider);
+  // This "faking" of describe allows us to directly import some code from
+  // test files into storybook.
+  // The idea is to create a default component that will be first tested,
+  // then imported in storybook to be displayed.
+  const fakeTestSuitePath = path.join(__dirname, "fakeTestSuite.js");
+  const testSuiteProvider = new webpack.ProvidePlugin({
+    describe: [fakeTestSuitePath, "describe"]
+  });
+  config.plugins.push(testSuiteProvider);
 }
